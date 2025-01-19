ad26311e7ed20878d02542f174e3fa698c95a10b
Farindra968 <farindrabahadurbhandari@gmail.com>
2025-01-20 02:05:32 +0545

    Fix token storage in login action; update localStorage to use response.data.token

--- src/redux/auth/authAction.js
+++ src/redux/auth/authAction.js
@@ -1,7 +1,7 @@
 import { loginApi } from "@/services/api/authApi";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
-const { createAsyncThunk } = require("@reduxjs/toolkit");
 
 const loginUser = createAsyncThunk(
   "auth/login",
@@ -10,7 +10,7 @@ const loginUser = createAsyncThunk(
       const response = await loginApi(data);
       console.log(response);
 
-      localStorage.setItem("apiToken", response.token);
+      localStorage.setItem("apiToken", response.data.token);
         toast.success('Login Successfully');
       return response.data;
     } catch (error) {
